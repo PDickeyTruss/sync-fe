9d7366346111744084bd069a5f70302204567bec
Patrick Dickey <pdickey@truss.works>
2021-09-08 11:39:22 -0600

    Dead code cleanup

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -57,7 +57,7 @@ function Home(props) {
         <Employee {...props} />
       </Grid>
       <Grid item xs={12}>
-        <Department {...props} />{' '}
+        <Department {...props} />
       </Grid>
     </Grid>
   )
